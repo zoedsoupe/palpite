5dfcec92983c9f03d3bce509ed5d33a1befc4ab2
Zoey Pessanha <zoey.spessanha@zeetech.io>
2026-09-10 16:33:47 -0300

    feat: titles table migration

---
+++ priv/repo/migrations/20260729214249_create_titles_table.exs
@@ -0,0 +1,18 @@
+defmodule Palpite.Repo.Migrations.CreateTitlesTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:titles) do
+      add :tmdb_id, :string
+      add :type, :string
+      add :name, :string
+      add :year, :smallint
+      add :poster_path, :string
+      add :genres, {:array, :string}
+      add :like_count, :integer, default: 0
+      add :dislike_count, :integer, default: 0
+    end
+
+    create unique_index(:titles, [:tmdb_id])
+  end
+end
